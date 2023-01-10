a766a9161e6811aba88c40421ecb18fe1a654d28
Haruto Mori <baystarsman@outlook.jp>
2023-01-10 13:21:08 +0100

    Add str datatype variable

---
+++ 2-variables.py
@@ -0,0 +1,11 @@
+# variable = a contaianer for a value. Behaves as the value that it contains
+
+# strings = a series of characters
+firstName = "Haruto"
+lastName = "Mori"
+
+# print the datatype of the variable
+print(type(firstName))
+print(type(lastName))
+
+print("Hello, " + firstName + " " + lastName + "\n")
