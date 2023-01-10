86983cf43c5e49ac0f0493ed1e1231c25a840572
Haruto Mori <baystarsman@outlook.jp>
2023-01-10 17:52:36 +0100

    count up and show even numbers by using for loop

---
+++ 12-for_loop.py
@@ -0,0 +1,17 @@
+# for loop = a statement that will execute its block of code
+#            a limited amount of times
+
+
+# count up
+for i in range(10):
+    print(i)
+
+
+# count up between 50 and 100
+for i in range(50, 100 + 1):
+    print(i)
+
+
+# show even numbers between 0 and 100
+for i in range(0,100 + 1, 2):
+    print(i)
