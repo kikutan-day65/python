a79ffa1442a6a4c8b55cc57698bc1bf90448022e
Haruto Mori <baystarsman@outlook.jp>
2023-01-10 16:33:41 +0100

    slicing name to a fist name, last name, and funky name

---
+++ 8-string_sicing.py
@@ -0,0 +1,13 @@
+# slicing = create a substring by extractiong elements from another string
+#           indexing[] or slice()
+#           [start:stop:step]
+
+name = "Haruto Mori"
+
+firstName = name[:6] # name[0:6] works as the same!
+lastName = name[7:] # name[7:11] works as the same!
+funkyName = name[::2] # name[0:11:2] works as the same!
+
+print(firstName)
+print(lastName)
+print(funkyName)
