e0be767f8c1c00183c828143c5dc6f2738ba17d0
Haruto Mori <baystarsman@outlook.jp>
2023-01-10 14:53:47 +0100

    Add examples of type casting value

---
+++ 5-type_cast.py
@@ -0,0 +1,28 @@
+# type casting = convert the datatype of a value to another datatyape
+
+x = 1 # int
+y = 2.0 # float
+z = "3" #str
+print(type(x))
+print(x)
+
+print(type(y))
+print(y)
+
+print(type(z))
+print(z)
+
+print("----- converted ------")
+
+str_x = str(x)
+int_y = int(y)
+float_z = float(z)
+
+print(type(str_x))
+print(str_x)
+
+print(type(int_y))
+print(int_y)
+
+print(type(float_z))
+print(float_z)
