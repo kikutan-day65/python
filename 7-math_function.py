3ae59db00031dd053e25eb322acf56918b1f1e18
Haruto Mori <baystarsman@outlook.jp>
2023-01-10 16:04:24 +0100

    Add round(), ceil(), floor(), abs(), pow(), and sqrt()

---
+++ 7-math_function.py
@@ -0,0 +1,25 @@
+#some functions are in math module, so it's required to import it.
+import math
+
+pi = 3.14
+num = -100
+
+x, y, z = 1, 2, 3
+
+# round() = output should be 3
+print(round(pi))
+
+# ceil() = output should be 4
+print(math.ceil(pi))
+
+# floor() = output should be 3
+print(math.floor(pi))
+
+# abs() = output should be 100
+print(abs(num))
+
+# pow(A, B) = output should be A powered B
+print(pow(pi, 2))
+
+# sqrt() = output should be square root of 49
+print(math.sqrt(49))
