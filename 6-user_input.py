1b5274dbf0c3e3c8b4ce92ae5424ece473fee850
Haruto Mori <baystarsman@outlook.jp>
2023-01-10 15:16:45 +0100

    create a file for math function

--- 6-user_input.py
+++ 6-user_input.py
@@ -1,7 +1,7 @@
 # input() = allows user to input and take it as a string. you have to surround datatype such as int(), float() if you want a specific datatype
 name = input("What is your name?: ")
 
-# it returns error if user typed diffrent datattype
+# it returns error if user typed diffrent datatype
 age = int(input("How old are you?: "))
 height = float(input("How tall are you?: "))
 
