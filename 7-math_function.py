c11a2868c6ad9257b0b2a056274968a21d8f3fd2
Haruto Mori <baystarsman@outlook.jp>
2023-01-10 16:09:12 +0100

    Add min() and max()

--- 7-math_function.py
+++ 7-math_function.py
@@ -9,17 +9,32 @@ x, y, z = 1, 2, 3
 # round() = output should be 3
 print(round(pi))
 
+
 # ceil() = output should be 4
 print(math.ceil(pi))
+
 
 # floor() = output should be 3
 print(math.floor(pi))
 
-# abs() = output should be 100
+
+# abs() = returns absolute value. output should be 100
 print(abs(num))
+
 
 # pow(A, B) = output should be A powered B
 print(pow(pi, 2))
 
+
 # sqrt() = output should be square root of 49
 print(math.sqrt(49))
+
+
+# max() = returns the maximum value
+max = max(x, y, z)
+print("max value is : " + str(max))
+
+
+# min() = returns the minimum value 
+min = min(x, y, z)
+print("min value is : " + str(min))
