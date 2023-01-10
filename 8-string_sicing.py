88216aad2436ff9a461733fd0358eec5fe41f8dc
Haruto Mori <baystarsman@outlook.jp>
2023-01-10 16:35:22 +0100

    reverse the name

--- 8-string_sicing.py
+++ 8-string_sicing.py
@@ -8,6 +8,9 @@ firstName = name[:6] # name[0:6] works as the same!
 lastName = name[7:] # name[7:11] works as the same!
 funkyName = name[::2] # name[0:11:2] works as the same!
 
+reversedName = name[::-1]
+
 print(firstName)
 print(lastName)
 print(funkyName)
+print(reversedName)
