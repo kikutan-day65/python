b92c98ff92288f0c4770b661a1ff23bb8c933500
Haruto Mori <baystarsman@outlook.jp>
2023-01-10 18:24:34 +0100

    printing rectangles by using nested for loop

---
+++ 13-nested_loop.py
@@ -0,0 +1,11 @@
+# neseted loop = The inner loop will finish all of its iterations before
+#                finishing one interation of the code
+
+rows = int(input("How many rows?: "))
+columns = int(input("How many columns?: "))
+symbol = input("Enter a symbol to use: ")
+
+for i in range(rows):
+    for j in range(columns):
+        print(symbol, end="")
+    print()
