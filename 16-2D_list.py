4813c15aff3683b14a26ded94b2a962ef83c5899
Haruto Mori <baystarsman@outlook.jp>
2023-01-10 23:26:48 +0100

    print out the elements in 2d list

---
+++ 16-2D_list.py
@@ -0,0 +1,24 @@
+# 2D list (mutidimensional list) = a list of list
+
+drinks = ["coffee", "soda", "tea"]
+dinner = ["pizza", "hamburger", "hotdog"]
+dessert = ["cake", "ice cream"]
+
+food = [drinks, dinner, dessert]
+
+# show elements in 2d list
+print(food)
+
+# output shold be elements in drinks
+print(food[0])
+
+# output should be hotdog
+print(food[1][2])
+
+print()
+
+# show all of the elements in food 2d list by using nested for loops
+for menu in food:
+    for item in menu:
+        print(item)
+    print()
