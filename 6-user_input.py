085b31c06a5ce61a21401d3b8f3213b0d90f8084
Haruto Mori <baystarsman@outlook.jp>
2023-01-10 15:13:19 +0100

    Add input() and examples

---
+++ 6-user_input.py
@@ -0,0 +1,11 @@
+# input() = allows user to input and take it as a string. you have to surround datatype such as int(), float() if you want a specific datatype
+name = input("What is your name?: ")
+
+# it returns error if user typed diffrent datattype
+age = int(input("How old are you?: "))
+height = float(input("How tall are you?: "))
+
+
+print("Hello, " + name)
+print("You're " + str(age) + " years old")
+print("You're " + str(height) + " cm")
