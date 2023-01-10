33dde26fb5c2fb088ed7851eee2d7b56909dc84e
Haruto Mori <baystarsman@outlook.jp>
2023-01-10 18:12:14 +0100

    fix the print() to show elements in oneline

--- 12-for_loop.py
+++ 12-for_loop.py
@@ -4,27 +4,34 @@
 
 # count up
 for i in range(10):
-    print(i)
+    print(i, end=" ")
 
+print("\n")
 
 # count up between 50 and 100
 for i in range(50, 100 + 1):
-    print(i)
+    print(i, end=" ")
 
+print("\n")
 
 # show even numbers between 0 and 100
 for i in range(0,100 + 1, 2):
-    print(i)
+    print(i, end = " ")
 
+print("\n")
 
 # show each letter of a string
 word = "pneumonoultramicroscopicsilicovolcanoconiosis"
 count = 1
 
 for i in word:
-    print(str(count) + ": " + i)
-    count += 1
+    if count != len(word):
+        print(str(count) + ": " + i, end = ", ")
+        count += 1
+    else:
+        print(str(count) + ": " + i)
 
+print("\n")
 
 # count down = range(start, stop, step)
 for sec in range(10, 0, -1):
