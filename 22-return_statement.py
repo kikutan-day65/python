72612be5ae7609d35680f607739e32a77dfd7781
Haruto Mori <baystarsman@outlook.jp>
2023-01-11 13:55:38 +0100

    create a function with return value

--- 22-return_statement.py
+++ 22-return_statement.py
@@ -1,3 +1,15 @@
 # return statement = functions send python values/pbjects back to the caller.
 #                    these values/objects are known as the function's return value
 
+def multiply(num1, num2):
+    return num1 * num2
+
+answer = multiply(6, 8)
+print(answer)
+
+
+number1 = int(input("Enter a first number: "))
+number2 = int(input("Enter a second number: "))
+
+answer = multiply(number1, number2)
+print(answer)
