dfbfa158f32e2d74d929b9f63ba65bc2e68c0650
Haruto Mori <baystarsman@outlook.jp>
2023-01-11 13:58:55 +0100

    create a file for keyword argument

---
+++ 23-keyword_arguments.py
@@ -0,0 +1,3 @@
+# keyword arguments = arguments preceeded by an identifier when we pass them to a function
+#                     The order of  the arguments doesn't matter, unlike positional arguments
+#                     Python knows the name of the arguments that our function receives
