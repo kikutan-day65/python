564fd6f3f2a23d2536990f9501b341a4995f4854
Haruto Mori <baystarsman@outlook.jp>
2023-01-11 14:04:40 +0100

    Add an example of keyword arguments and how it works

--- 23-keyword_arguments.py
+++ 23-keyword_arguments.py
@@ -1,3 +1,11 @@
 # keyword arguments = arguments preceeded by an identifier when we pass them to a function
 #                     The order of  the arguments doesn't matter, unlike positional arguments
 #                     Python knows the name of the arguments that our function receives
+
+
+
+def hello(first, middle, last):
+    print("Hello, " + first + " " + middle + " " + last)
+
+# arguments can be written in unordered ny using keyword argument
+hello(last="Mori", first="Haruto", middle="Praha")
