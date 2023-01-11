58861831cf3f69e339ed09abb35a3c9e7c7ec6d9
Haruto Mori <baystarsman@outlook.jp>
2023-01-11 11:53:18 +0100

    change variable names

--- 16-2D_list.py
+++ 16-2D_list.py
@@ -5,21 +5,21 @@ dinner = ["pizza", "hamburger", "hotdog"]
 dessert = ["cake", "ice cream"]
 
 # conteanate each list
-food = [drinks, dinner, dessert]
+menus = [drinks, dinner, dessert]
 
 # show elements in 2d list
-print(food)
+print(menus)
 
 # output shold be elements in drinks
-print(food[0])
+print(menus[0])
 
 # output should be hotdog
-print(food[1][2])
+print(menus[1][2])
 
 print()
 
 # show all of the elements in food 2d list by using nested for loops
-for menu in food:
-    for item in menu:
-        print(item)
+for menu in menus:
+    for food in menu:
+        print(food)
     print()
