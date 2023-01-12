8c455bafcf264c4b4b7c7a922a378198ddda26a7
Haruto Mori <baystarsman@outlook.jp>
2023-01-12 15:05:23 +0100

    create a file forvariable scope

---
+++ 25-variable_scope.py
@@ -0,0 +1,3 @@
+# scope = The regiion that a variable is recognized
+#         A variable is only available from inside the region it is created.
+#         A global and locally scoped versions of a variable can be created
