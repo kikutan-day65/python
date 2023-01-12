1e15dc1e6e356f5a31050c0b5ad7eff5acb8aa52
Haruto Mori <baystarsman@outlook.jp>
2023-01-12 18:54:50 +0100

    add and append a sentence into a file

--- basics/32-36-manage_files.py
+++ basics/32-36-manage_files.py
@@ -1,6 +1,18 @@
 
+# reading a file
 try:
     with open('C:\\Users\\bayst\\coding\\python\\files\\test.txt') as file:
         print(file.read())
 except FileNotFoundError:
     print("that file is not found")
+
+
+# add new sentences to the file
+text = "Haruto Mori\nFrom Japan\nI'm a big fan of the Baystars\n"
+with open('C:\\Users\\bayst\\coding\\python\\files\\test2.txt', 'w') as file2:
+    file2.write(text)
+
+# append new sentences to the file
+append = "!!!I'm studying pyhton in the national technical library in prague!!!"
+with open('C:\\Users\\bayst\\coding\\python\\files\\test2.txt', 'a') as file2:
+    file2.write(append)
