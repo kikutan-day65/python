e45a7051e52b84ca927b2ce0fb2bd6fc28d48e94
Haruto Mori <baystarsman@outlook.jp>
2023-01-12 15:17:58 +0100

    add an example for global scope with PI variable

--- 25-variable_scope.py
+++ 25-variable_scope.py
@@ -2,6 +2,7 @@
 #         A variable is only available from inside the region it is created.
 #         A global and locally scoped versions of a variable can be created
 
+import math
 
 # globalName can be accesses from anywhere
 globalName = "Yazan"
@@ -13,3 +14,20 @@ def display():
     print(globalName)
 
 display()
+
+#global scope 
+PI = 3.14
+
+def area(dia):
+    return pow(dia, 2) * PI
+
+def circumference(dia):
+    return (dia * 2) * PI
+
+diameter = float(input("Enter the length of diameter: "))
+
+area = area(diameter)
+circum = circumference(diameter)
+
+print(area)
+print(circum)
