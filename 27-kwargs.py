6611fddb31855ca60c61881f7ef9be4a720fb469
Haruto Mori <baystarsman@outlook.jp>
2023-01-12 16:17:06 +0100

    add an example of kwargs, kwargs takes all input as a dictinoary{key:value}

--- 27-kwargs.py
+++ 27-kwargs.py
@@ -1,3 +1,20 @@
-# **kwawrgs = parameter that will pack all arguments into a dictionary
+# **kwawrgs = parameter that will pack all arguments into a DICTIONARY
 #             useful so that a function can accept a varying amount of keyword arguments
 
+"""
+def hello(first, last):
+    print("Hello," + first + " " + last)
+
+hello(first="Haruto", middle="Praha", last="Mori")
+
+this gives a typeError
+"""
+
+def hello(**kwargs):
+    print("Hello," + kwargs['first'] + " " + kwargs['last'])
+
+    print("Hello,", end=" ")
+    for key, value in kwargs.items():
+        print(value, end=" ")
+
+hello(title="Ing.", first="Haruto", middle="Praha", last="Mori")
