5deb5091136a23fc392314d00c07aea21399213f
Haruto Mori <baystarsman@outlook.jp>
2023-01-12 18:58:53 +0100

    add new line for the readability

--- basics/32-36-manage_files.py
+++ basics/32-36-manage_files.py
@@ -9,10 +9,13 @@ except FileNotFoundError:
 
 # add new sentences to the file
 text = "Haruto Mori\nFrom Japan\nI'm a big fan of the Baystars\n"
+
 with open('C:\\Users\\bayst\\coding\\python\\files\\test2.txt', 'w') as file2:
     file2.write(text)
 
+
 # append new sentences to the file
 append = "!!!I'm studying pyhton in the national technical library in prague!!!"
+
 with open('C:\\Users\\bayst\\coding\\python\\files\\test2.txt', 'a') as file2:
     file2.write(append)
