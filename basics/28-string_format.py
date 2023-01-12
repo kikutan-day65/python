5c8b348f6d155a99034e5c1393a4f3263ffe1749
Haruto Mori <baystarsman@outlook.jp>
2023-01-12 16:26:26 +0100

    add .format() and how it works

--- basics/28-string_format.py
+++ basics/28-string_format.py
@@ -1,2 +1,11 @@
 # str.format() = optional method that gives users
 #                more control when displaying output
+
+
+animal = "dog"
+item = "moon"
+
+print("The " + animal + " jumped over the " + item)
+
+# same output above, used placeholder{}
+print("The {} jumped over the {}".format(animal, item))
