6c64ed7c0ffc7961d0975c61aa1db9e9291eb302
Haruto Mori <baystarsman@outlook.jp>
2023-01-12 20:33:44 +0100

    add example of copying a file but permissionError occurs

--- basics/34-copy_files.py
+++ basics/34-copy_files.py
@@ -1,3 +1,12 @@
 # copyfile() = copies contents of a file
 # copy() = copyfile() + permission mode + destination can be a dictionary
 # copy2() = copy() + copies metadata (file's creation and modification times)
+
+# Shutil module offers high-level operation on a file like a copy, create, and remote operation on the file.
+import shutil
+
+src = 'C:\\Users\\bayst\\coding\\python\\files\\test3.txt'
+dst = 'C:\\Users\\bayst\\coding\\python\\basics'
+
+# .copyfile(src, dst)
+shutil.copyfile(src, dst)
