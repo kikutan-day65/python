3934479cb3f7b62f3e77a45f6fe83402d1fc76ac
Haruto Mori <baystarsman@outlook.jp>
2023-01-12 15:11:48 +0100

    see the difference global and local scoped variable

--- 25-variable_scope.py
+++ 25-variable_scope.py
@@ -1,3 +1,15 @@
 # scope = The regiion that a variable is recognized
 #         A variable is only available from inside the region it is created.
 #         A global and locally scoped versions of a variable can be created
+
+
+# globalName can be accesses from anywhere
+globalName = "Yazan"
+
+def display():
+    # localName cannot be accessed from the outside of function
+    localName = "Haruto"
+    print(localName)
+    print(globalName)
+
+display()
