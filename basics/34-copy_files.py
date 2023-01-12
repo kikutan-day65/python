9c073d373c83afac9d1288cf42388b1f70263056
Haruto Mori <baystarsman@outlook.jp>
2023-01-12 20:23:15 +0100

    create a file for copy a file

---
+++ basics/34-copy_files.py
@@ -0,0 +1,3 @@
+# copyfile() = copies contents of a file
+# copy() = copyfile() + permission mode + destination can be a dictionary
+# copy2() = copy() + copies metadata (file's creation and modification times)
