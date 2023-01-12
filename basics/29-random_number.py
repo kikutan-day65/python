deeb87f2b7c6ea2d01a43e9b16a30735e4085276
Haruto Mori <baystarsman@outlook.jp>
2023-01-12 17:38:01 +0100

    add choice() and shuffle()

--- basics/29-random_number.py
+++ basics/29-random_number.py
@@ -7,3 +7,14 @@ print(x)
 # generating random floating number
 y = random.random()
 print(y)
+
+# .choice() = chooses somrthing in the list
+myList = ['rock', 'paper', 'scissors']
+z = random.choice(myList)
+print(z)
+
+# .shuffle() = shuffles the elemnt in the list 
+cards = ["A",2,3,4,5,6,7,8,9,"J","Q", "K"]
+random.shuffle(cards)
+
+print(cards)
