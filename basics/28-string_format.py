408b820e5f1c856e39fd8c3fffa402b1804c1fb5
Haruto Mori <baystarsman@outlook.jp>
2023-01-12 17:29:27 +0100

    add how to display number as bin, oct, hex, and scientific

--- basics/28-string_format.py
+++ basics/28-string_format.py
@@ -29,3 +29,23 @@ print("my favorite book is {:>20}. What about you?".format(book))
 PI = 3.14159
 # displays only two digits
 print("The number PI is {:.2}".format(PI))
+
+# diaplay only decimal two points 
+print("The number PI is {:.2f}".format(PI))
+
+
+num = 11200
+# dispaly number with , at the thousand or more posintion 
+print("The number is {:,}".format(num))
+
+# display number as binary
+print("The number is {:b}".format(num))
+
+# display number as octal
+print("The number is {:o}".format(num))
+
+# display number as hexadecimal
+print("The number is {:X}".format(num))
+
+# display number with a scientific notation
+print("The number is {:e}".format(num))
