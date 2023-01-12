a612c00970814c8db77077643c4946b691633ce1
Haruto Mori <baystarsman@outlook.jp>
2023-01-12 15:52:44 +0100

    add new comment

--- 26-args.py
+++ 26-args.py
@@ -1,3 +1,25 @@
 # *args = parameter that will pack all arguments into a tuple
 #         useful so that a function can accept a varying amount of arguments
 
+"""
+def add(num1, num2):
+    sum = num1 + num2
+    return sum
+
+print(add(1,2,3))
+
+This gives a typeError
+"""
+
+# *args argument takes all the input as a tuples, *args can be changed any name
+def add(*args):
+    # shows a tuple
+    print(args)
+
+    sum = 0
+    for i in args:
+        sum += i
+    return sum
+
+print(add(1, 2, 3))
+print(add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
