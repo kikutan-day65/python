1660e01ebd76b20282d286a12d0ec98318d59c2a
Haruto Mori <baystarsman@outlook.jp>
2023-01-13 11:51:34 +0100

    add new examples of class

--- advanced/class.py
+++ advanced/class.py
@@ -1,15 +1,21 @@
 # OOP
 
 class PlayerCharacter:
+    # class object attribute
+    membership = True
     def __init__(self, name, age):
         # creating an attribute and set what parameter will be assigned
         # self refers to a variable that coders created (player1, player2)
-        self.name = name
-        self.age = age
+        if (PlayerCharacter.membership):
+            self.name = name
+            self.age = age
     
     def run(self):
         print("run")
         return "done"
+    
+    def shout(self):
+        print(f"my name is {self.name}")
 
 # instances
 player1 = PlayerCharacter("Haruto", 24)
@@ -19,8 +25,11 @@ print(player1) # shows where the object player1 is stored at the memory
 print(player1.name)
 print(player1.age)
 print(player1.run())
+print(player1.membership)
+print(player1.shout())
 
 print(player2) # shows where the object player2 is stored at the memory
 print(player2.name)
 print(player2.age)
 print(player2.run())
+print(player1.shout())
