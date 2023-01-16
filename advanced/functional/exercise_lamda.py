181321e648ee8eb027e04ff6ae67f241a6a11095
Haruto Mori <baystarsman@outlook.jp>
2023-01-16 15:29:20 +0100

    solve the first exercise

---
+++ advanced/functional/exercise_lamda.py
@@ -0,0 +1,10 @@
+# wirte a code to get numbers squared with lamda expression
+my_list = [5,4,3]
+
+result = list(map(lambda item: item ** 2, my_list))
+print(result)
+
+
+# sort the list based on the 2nd value of the tuple with lamda expression
+a = [(0,2), (4,3), (9,9), (10, -1)]
+
