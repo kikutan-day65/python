304b9e4819ccb14f69eccf47058e7985299487bf
Haruto Mori <baystarsman@outlook.jp>
2023-01-16 15:36:04 +0100

    solve the 2nd exercise

--- advanced/functional/exercise_lamda.py
+++ advanced/functional/exercise_lamda.py
@@ -6,5 +6,7 @@ print(result)
 
 
 # sort the list based on the 2nd value of the tuple with lamda expression
-a = [(0,2), (4,3), (9,9), (10, -1)]
+a = [(4,3), (0,2), (9,9), (10, -1)]
 
+a.sort(key=lambda tpl: tpl[1])
+print(a)
