dfe335cbb571e7d9596b5b7ca9d8beba9866063f
Haruto Mori <baystarsman@outlook.jp>
2023-01-17 13:56:31 +0100

    fix some lines

--- module/main.py
+++ module/main.py
@@ -1,6 +1,6 @@
 import utility
-from shopping import shopping_cart
+import shopping.shopping_cart
 
 print(utility.divide(2,3))
 
-print(shopping_cart.buy('apple'))
+print(shopping.shopping_cart.buy('apple'))
