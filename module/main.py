0d4d985c54cae8d06de46d6e83258a0fd51339d9
Haruto Mori <baystarsman@outlook.jp>
2023-01-17 14:15:55 +0100

    add example of if __name__ == '__main__':

--- module/main.py
+++ module/main.py
@@ -4,3 +4,10 @@ from shopping import shopping_cart
 print(divide(2,3))
 
 print(shopping_cart.buy('apple'))
+
+
+# only run code of the main file
+print(__name__)
+if __name__ == '__main__':
+    print("please run this")
+    print(multiply(3,4))
