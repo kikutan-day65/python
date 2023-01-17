887d68ed661ee2f5722a6f6abf3f2e7851677c90
Haruto Mori <baystarsman@outlook.jp>
2023-01-17 16:59:55 +0100

    give examples of datetime module and array module

---
+++ module/datetime_array_module.py
@@ -0,0 +1,14 @@
+import datetime
+
+# datetime.time(h, m, s)
+print(datetime.time(5,45,2))
+
+# shows today
+print(datetime.date.today())
+
+
+from array import array
+
+arr = array('i', [1,2,3])
+
+print(arr[0])
