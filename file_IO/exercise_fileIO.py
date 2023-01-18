79dc95c43f590e8cbfa8d6de7a114edbd99009d0
Haruto Mori <baystarsman@outlook.jp>
2023-01-18 16:45:59 +0100

    translation exercise, but translated into garbled text

--- file_IO/exercise_fileIO.py
+++ file_IO/exercise_fileIO.py
@@ -1,6 +1,14 @@
+from translate import Translator
+
+translator= Translator(to_lang="ja")
 try:
     with open("./exercise.txt", mode='r') as my_file:
-        print(my_file.read())
+        text = my_file.read()
+        translation = translator.translate(text)
+        
+        with open("./translated.txt", mode='w') as my_file2:
+            # expected to be translated into japanese, but garbled text. why?
+            my_file2.write(translation)
 
 except FileNotFoundError as e:
     print("check your file path")
