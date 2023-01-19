c5657c92c572ee9956baa9b0c78367031cc7a6ef
Haruto Mori <baystarsman@outlook.jp>
2023-01-19 13:59:50 +0100

    add an example of testing

---
+++ testing/test.py
@@ -0,0 +1,15 @@
+import unittest
+import main
+
+class TestMain(unittest.TestCase):
+    def test_do_stuff(self):
+        test_param = 10
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 15)
+    
+    def test_do_stuff2(self):
+        test_param = "jdgkajgfa"
+        result = main.do_stuff(test_param)
+        self.assertTrue(isinstance(result, ValueError))
+
+unittest.main()
