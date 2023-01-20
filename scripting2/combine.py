07c538f27a33c69bd2d3e5512ac6d9aa6f43faa8
Haruto Mori <baystarsman@outlook.jp>
2023-01-20 17:57:52 +0100

    change the path to store the super.pdf

--- scripting2/combine.py
+++ scripting2/combine.py
@@ -12,6 +12,6 @@ def pdf_combiner(pdf_list):
     merger = PyPDF2.PdfFileMerger()
     for pdf in pdf_list:
         merger.append(pdf)
-    merger.write("super.pdf")
+    merger.write(".\\pdfs\\super.pdf")
 
 pdf_combiner(inputs)
