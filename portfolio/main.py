6cf2d0a4793e2ac06ac1d3b1d02ae9df640dd17e
Haruto Mori <baystarsman@outlook.jp>
2023-01-22 16:28:19 +0100

    wrote a code as it works only by one block

--- portfolio/main.py
+++ portfolio/main.py
@@ -6,14 +6,18 @@ app = Flask(__name__)
 def my_home():
     return render_template("index.html")
 
-@app.route("/about.html")
-def about():
-    return render_template("about.html")
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
 
-@app.route("/works.html")
-def work():
-    return render_template("works.html")
+# @app.route("/about.html")
+# def about():
+#     return render_template("about.html")
 
-@app.route("/contact.html")
-def contact():
-    return render_template("contact.html")
+# @app.route("/works.html")
+# def work():
+#     return render_template("works.html")
+
+# @app.route("/contact.html")
+# def contact():
+#     return render_template("contact.html")
