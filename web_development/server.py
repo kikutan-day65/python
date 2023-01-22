8fd98116a50754c2e144b92c3957126c4042852e
Haruto Mori <baystarsman@outlook.jp>
2023-01-22 15:36:39 +0100

    modify the py and html files

--- web_development/server.py
+++ web_development/server.py
@@ -3,17 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 print(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+@app.route("/<username>/<int:post_id>")
+def hello_world(username=None, post_id=None):
+    return render_template("index.html", name=username, post_id=post_id)
 
 @app.route("/about.html")
 def about():
     return render_template("about.html")
 
-@app.route("/blog")
-def blog():
-    return "this is my blog"
 
 @app.route("/blog/2023/dogs")
 def blog2():
