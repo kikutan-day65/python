5bc01e4acbd03a55d77aafeaf51a1907e7aa214b
Haruto Mori <baystarsman@outlook.jp>
2023-01-22 15:03:19 +0100

    render a html file

--- web_development/server.py
+++ web_development/server.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 print(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Ohayo, Haruto Mori!</p>"
+    return render_template("index.html")
+
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+
+@app.route("/blog")
+def blog():
+    return "this is my blog"
+
+@app.route("/blog/2023/dogs")
+def blog2():
+    return "this is my dog"
