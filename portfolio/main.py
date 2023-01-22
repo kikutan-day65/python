88d6aa9bf7cdbc75d48604901536bcc70a737950
Haruto Mori <baystarsman@outlook.jp>
2023-01-22 18:34:40 +0100

    add try and except statement for database

--- portfolio/main.py
+++ portfolio/main.py
@@ -28,16 +28,21 @@ def write_to_csv(data):
         email = data["email"]
         subject = data["subject"]
         message = data["message"]
-        csv_writer = csv.writer(database2, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL)
+        csv_writer = csv.writer(database2, delimiter=",",
+                                quotechar="|", quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([email, subject, message])
+
 
 @app.route("/submit_form", methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-        data = request.form.to_dict()
-        write_to_file(data)
-        write_to_csv(data)
-        return redirect("thankyou.html")
+        try:
+            data = request.form.to_dict()
+            write_to_file(data)
+            write_to_csv(data)
+            return redirect("thankyou.html")
+        except:
+            return "did not save to database"
     else:
         return 'somthing went wrong. Try again!'
 
