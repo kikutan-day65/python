bcab8e881301baf45c27f68c3a6f44d5a8c7cf50
Haruto Mori <baystarsman@outlook.jp>
2023-01-22 16:18:57 +0100

    modify the templates

--- portfolio/main.py
+++ portfolio/main.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def my_home():
     return render_template("index.html")
+
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+
+@app.route("/works.html")
+def work():
+    return render_template("works.html")
