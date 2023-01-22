9d4e45d8c42b96408446ff36771cda8cbb30d143
Haruto Mori <baystarsman@outlook.jp>
2023-01-22 11:50:38 +0100

    add feature to scrape more pages

--- web_scraping/scrape.py
+++ web_scraping/scrape.py
@@ -3,9 +3,18 @@ from bs4 import BeautifulSoup
 import pprint
 
 res = requests.get("https://news.ycombinator.com/news")
+res2 = requests.get("https://news.ycombinator.com/news?p=2")
+
 soup = BeautifulSoup(res.text, "html.parser") # parsing, most of time(99%) we usually use this 
+soup2 = BeautifulSoup(res2.text, "html.parser")
+
 links = soup.select('.titleline')
 subtext = soup.select('.subtext')
+links2 = soup2.select('.titleline')
+subtext2 = soup2.select('.subtext')
+
+mega_links = links + links2
+mega_subtext = subtext + subtext2
 
 def sort_stories_by_votes(hnlist):
     return sorted(hnlist, key=lambda k:k["votes"], reverse=True)
@@ -23,4 +32,4 @@ def create_custom_hn(links, subtext):
                 hn.append({"title": title, "link": href, "votes": points})
     return sort_stories_by_votes(hn)
 
-pprint.pprint(create_custom_hn(links, subtext))
+pprint.pprint(create_custom_hn(mega_links, mega_subtext))
