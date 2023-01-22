1566c87dd8897ec946284dea38d03fb056bfb3fd
Haruto Mori <baystarsman@outlook.jp>
2023-01-22 18:24:04 +0100

    fix the code

--- portfolio/main.py
+++ portfolio/main.py
@@ -24,11 +24,11 @@ def write_to_file(data):
 
 
 def write_to_csv(data):
-    with open('database.csv', mode='a') as database2:
+    with open('database.csv', newline='', mode='a') as database2:
         email = data["email"]
         subject = data["subject"]
         message = data["message"]
-        csv_writer = csv.writer(database2, delimiter=",", quotechar="|", newline=" ", quoting=csv.QUOTE_MINIMAL)
+        csv_writer = csv.writer(database2, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([email, subject, message])
 
 @app.route("/submit_form", methods=['POST', 'GET'])
