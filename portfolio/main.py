b0a230f3dbe4c2b48f116bcf1b683cf0e3d40ec9
Haruto Mori <baystarsman@outlook.jp>
2023-01-22 18:02:23 +0100

    add a txt file to store the data from the contact

--- portfolio/main.py
+++ portfolio/main.py
@@ -10,11 +10,18 @@ def my_home():
 def html_page(page_name):
     return render_template(page_name)
 
+def write_to_file(data):
+    with open('database.txt', mode='a') as databese:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = databese.write(f"\n{email}, {subject}, {message}")
+
 @app.route("/submit_form", methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
+        write_to_file(data)
         return redirect("thankyou.html")
     else:
         return 'somthing went wrong. Try again!'
